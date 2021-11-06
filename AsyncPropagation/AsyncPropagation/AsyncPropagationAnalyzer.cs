766e6a1dcee9044b1ad64a7fb98aa53bfea92da8
gbtb <goodbetterthebeast3@gmail.com>
2021-11-06 18:18:12 +1000

    trying to use analyzer + codefix

--- AsyncPropagation/AsyncPropagation/AsyncPropagationAnalyzer.cs
+++ AsyncPropagation/AsyncPropagation/AsyncPropagationAnalyzer.cs
@@ -1,20 +1,13 @@
 ﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.FindSymbols;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
 
 namespace AsyncPropagation
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class AsyncPropagationAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "AsyncPropagation";
+        public const string DiagnosticId = "Async01";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
@@ -23,9 +16,9 @@ namespace AsyncPropagation
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AsyncAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -40,13 +33,10 @@ namespace AsyncPropagation
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
             var methodSymbol = (IMethodSymbol)context.Symbol;
-            if (methodSymbol.IsAsync)
-            {
-                //cant use SymbolFinder in Analyzer, since we have no Solution in here. So we just guessing
-                var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], methodSymbol.Name);
+            //cant use SymbolFinder in Analyzer, since we have no Solution in here. So we just guessing
+            var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], methodSymbol.Name);
 
-                context.ReportDiagnostic(diagnostic);
-            }
+            context.ReportDiagnostic(diagnostic);
         }
     }
 }
